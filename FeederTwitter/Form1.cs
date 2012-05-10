0056e067a376474a2369ac2c5d8af5efbdbe81f1
Tom syamoji <uniquequeue@gmail.com>
2012-05-11 03:00:21 +0900

    VS C# auto creation file added

---
+++ FeederTwitter/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FeederTwitter
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
